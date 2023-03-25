d866da8a193b5974598ada23be3e776f6248a558
Nickylood <110985074+Nickylood@users.noreply.github.com>
2023-03-25 22:05:52 +0100

    12

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,32 @@ const { dirname } = require('path');
 const path = require('path');
 
 module.exports = {
+    mode: 'production',
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
-
     }
+}
+
+devServer: {
+    static: [
+        {
+            directory: path.join(__dirname, 'public'),
+        },
+        {
+            directory: path.join(__dirname, 'js'),
+            watch: true,
+        },
+        {
+            directory: path.join(__dirname, 'src'),
+            watch: true,
+        },
+    ]
+    client: {
+        progress: true
+    }
+
+    compress: true
+    port: 9000
 }
